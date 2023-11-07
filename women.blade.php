023756cfcf06e984c7028c2d9bb707f153a7c4de
Sajrul01 <112762621+Sajrul01@users.noreply.github.com>
2023-11-07 06:14:20 +0530

    Add files via upload

---
+++ women.blade.php
@@ -0,0 +1,38 @@
+<main>
+    <section class="product">
+        <img src="images\dress.jpg" alt="dress">
+        
+        <h2>Maha loup</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac consectetur purus.</p>
+        <p class="price">$19.99</p>
+        <button>Add to Cart</button>
+    </section>
+
+    <section class="product">
+        <img src="images\dress2.jpg" alt="dress2">
+        <h2>Paliouium liupio</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac consectetur purus.</p>
+        <p class="price">$29.99</p>
+        <button>Add to Cart</button>
+    </section>
+
+
+    <section class="product">
+        <img src="images\dress3.jpg" alt="dress3">
+        <h2>Paliouium liupio</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac consectetur purus.</p>
+        <p class="price">$29.99</p>
+        <button>Add to Cart</button>
+    </section>
+
+    <section class="product">
+        <img src="images\dress.jpg" alt="dress">
+        
+        <h2>Maha loup</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac consectetur purus.</p>
+        <p class="price">$19.99</p>
+        <button>Add to Cart</button>
+    </section>
+
+    <!-- Add more product sections as needed -->
+</main>
